696352f5635e31fcab7c26002efdfbdf959bcd9d
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-01-13 20:06:55 -0300

    adding model.ts; adding todo and todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
 const App:React.FC = () => {
 
-  const [todo, setTodo] = useState<string>('');
+  const [ todo, setTodo ] = useState<string>('');
+  const [ todos, setTodos ] = useState<Todo[]>([]);
 
-  console.log(todo);
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos( [ ...todos, { id: Date.now(), todo, isDone:false } ] )
+      setTodo('');
+    }
+    
+  };
+
+  console.log(todos);
   
 
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
-      <InputFeild todo={todo} setTodo={setTodo} />
+      <InputFeild 
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
 
     </div>
   );
