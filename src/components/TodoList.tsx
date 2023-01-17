92b58a92e749cd3e9f4d03a75ca7fbc5e8cd3576
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-01-16 21:27:32 -0300

    adding styles

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,24 +3,43 @@ import { Todo } from '../model'
 import SingleTodo from './SingleTodo';
 import './styles.css'
 
-interface Props{
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ( { todos, setTodos } ) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className='todos'>
-      {todos.map(todo => (
-        <SingleTodo
-            todo={ todo }
-            key={ todo.id }
-            todos={ todos }
-            setTodos={ setTodos }
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+        </span>
+        {todos.map((todo) =>
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        )}
+      </div>
+
+      <div className="todos remove">
+        <span className="todos__heading">
+          Completed Tasks
+        </span>
+        {todos.map((todo) =>
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default TodoList
